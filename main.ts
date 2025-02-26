042b0b9469a3295dc60d2f6ac0a4e25ac3efbf72
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-02-26 15:29:19 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,11 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.setLedColor(0x0000ff)
-    rs232.sendeText("rs232-41", 13)
+    rs232.sendeText("rs232-41")
     basic.turnRgbLedOff()
+})
+input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
+    rs232.empfangAbbrechen()
 })
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 rs232.setPins(DigitalPin.C17, AnalogPin.C16, 150)
