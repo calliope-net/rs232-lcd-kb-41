dbd1508088a831e2d07fc7ac9588d0d0b9efdff6
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-02-26 20:18:39 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,20 +5,16 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     keyboardCode = 0
     keyboardIndex = 0
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 15, lcd16x2rgb.lcd16x2_text(""))
-    sendKeyboard()
+    textEingeben()
+    textSenden()
     basic.turnRgbLedOff()
 })
-function sendKeyboard () {
-    while (keyboardCode != 13 && keyboardIndex < 16) {
-        keyboardCode = rs232.readCardKeyboard()
-        if (rs232.between(keyboardCode, 32, 127)) {
-            sendArray.push(keyboardCode)
-            lcd16x2rgb.setCursor(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, keyboardIndex)
-            lcd16x2rgb.writeLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), String.fromCharCode(keyboardCode))
-            keyboardIndex += 1
-        }
+function textSenden () {
+    if (sendArray.length > sendIndex) {
+        rs232.sendeAsc(sendArray[sendIndex])
+        lcd16x2rgb.setCursorCB(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, sendIndex, true, true)
+        sendIndex += 1
     }
-    sendArray.push(13)
 }
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     while (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
@@ -37,6 +33,18 @@ input.onButtonEvent(Button.B, ButtonEvent.Hold, function () {
     rs232.empfangAbbrechen(true)
     basic.turnRgbLedOff()
 })
+function textEingeben () {
+    while (keyboardCode != 13 && keyboardIndex < 16) {
+        keyboardCode = rs232.readCardKeyboard()
+        if (rs232.between(keyboardCode, 32, 127)) {
+            sendArray.push(keyboardCode)
+            lcd16x2rgb.setCursor(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, keyboardIndex)
+            lcd16x2rgb.writeLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), String.fromCharCode(keyboardCode))
+            keyboardIndex += 1
+        }
+    }
+    sendArray.push(13)
+}
 let keyboardIndex = 0
 let keyboardCode = 0
 let sendIndex = 0
@@ -44,9 +52,3 @@ let sendArray: number[] = []
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 rs232.setPins(DigitalPin.C17, AnalogPin.C16, 150)
 rs232.setTakt(400)
-loops.everyInterval(500, function () {
-    if (sendArray.length > sendIndex) {
-        rs232.sendeAsc(sendArray[sendIndex])
-        sendIndex += 1
-    }
-})
